4a9e18ce8698ae64b5833f710b30ef36a8961be7
d1y <chenhonzhou@gmail.com>
2024-08-03 23:59:00 +0800

    cu: miss light CuColors
    补充亮色

--- lib/colors.dart
+++ lib/colors.dart
@@ -3,20 +3,86 @@
 import 'package:flutter/widgets.dart';
 
 class CuColors {
-  static const yellow = Color(0xfffbbd08);
-  static const orange = Color(0xfff37b1d);
+  /// 嫣红
   static const red = Color(0xffe54d42);
-  static const pink = Color(0xffe03997);
-  static const mauve = Color(0xffb745cb);
-  static const purple = Color(0xff8044de);
-  static const blue = Color(0xff0081ff);
-  static const cyan = Color(0xff37c0fe);
-  static const green = Color(0xff3eb93b);
+
+  /// 桔橙
+  static const orange = Color(0xfff37b1d);
+
+  /// 明黄
+  static const yellow = Color(0xfffbbd08);
+
+  /// 橄榄
   static const olive = Color(0xff8dc63f);
-  static const grey = Color(0xff8799a3);
+
+  /// 森绿
+  static const green = Color(0xff39b54a);
+
+  /// 天青
+  static const cyan = Color(0xff1cbbb4);
+
+  /// 海蓝
+  static const blue = Color(0xff0081ff);
+
+  /// 姹紫
+  static const purple = Color(0xff6739b6);
+
+  /// 木槿
+  static const mauve = Color(0xff9c26b0);
+
+  /// 桃粉
+  static const pink = Color(0xffe03997);
+
+  /// 棕褐
   static const brown = Color(0xffa5673f);
-  static const black = Color(0xff000000);
+
+  /// 玄灰
+  static const grey = Color(0xff8799a3);
+
+  /// 草灰
+  static const gray = Color(0xffaaaaaa);
+
+  /// 墨黑
+  static const black = Color(0xff333333);
+
+  /// 雅白
   static const white = Color(0xffffffff);
+
+  /// 嫣红(亮色)
+  static const redLight = Color(0xfffadbd9);
+
+  /// 桔橙(亮色)
+  static const orangeLight = Color(0xfffde6d2);
+
+  /// 明黄(亮色)
+  static const yellowLight = Color(0xFDF2CED2);
+
+  /// 橄榄(亮色)
+  static const oliveLight = Color(0xffe8f4d9);
+
+  /// 森绿(亮色)
+  static const greenLight = Color(0xD6F0DBFF);
+
+  /// 天青(亮色)
+  static const cyanLight = Color(0xffd2f1f0);
+
+  /// 海蓝(亮色)
+  static const blueLight = Color(0xffcce6ff);
+
+  /// 姹紫(亮色)
+  static const purpleLight = Color(0xffe1d7f0);
+
+  /// 木槿(亮色)
+  static const mauveLight = Color(0xffebd4ef);
+
+  /// 桃粉(亮色)
+  static const pinkLight = Color(0xfff9d7ea);
+
+  /// 棕褐(亮色)
+  static const brownLight = Color(0xffede1d9);
+
+  /// 玄灰(亮色)
+  static const greyLight = Color(0xffe7ebed);
 }
 
 class CuLinearGradient {
