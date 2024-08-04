ca31e354c53783c8296f747f15d170966286886f
d1y <chenhonzhou@gmail.com>
2024-08-05 00:41:43 +0800

    cu: add extensions

--- lib/extensions.dart
+++ lib/extensions.dart
@@ -1,5 +1,148 @@
 // https://pub.dev/packages/awesome_extensions
-import 'package:flutter/widgets.dart';
+import 'package:colorui/utils.dart' as utils;
+import 'dart:io' show Platform;
+import 'package:flutter/material.dart';
+
+extension JSArray<T> on List<T> {
+  /// https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+  List<T> filter(bool Function(T) test) {
+    return where(test).toList();
+  }
+
+  /// https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+  T find(bool Function(T) test, [T Function()? orElse]) {
+    return firstWhere(test, orElse: orElse);
+  }
+
+  /// https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Array/findIndex
+  int findIndex(bool Function(T) test, [int start = 0]) {
+    return indexWhere(test, start);
+  }
+
+  /// https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
+  bool includes(T item) {
+    return contains(item);
+  }
+
+  /// https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Array/pop
+  T pop() {
+    return removeLast();
+  }
+
+  /// https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Array/shift
+  T shift() {
+    return removeAt(0);
+  }
+
+  /// https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Array/unshift
+  unshift(T item) {
+    insert(0, item);
+  }
+
+  /// https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Array/reverse
+  List<T> reverse() {
+    return reversed.toList();
+  }
+
+  /// https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Array/push
+  push(T item) {
+    add(item);
+  }
+
+  /// https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
+  List<T> slice(int start, [int? end]) {
+    return sublist(start, end);
+  }
+
+  /// https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Array/some
+  bool some(bool Function(T) test) {
+    return any(test);
+  }
+
+  List<R> mapWithIndex<R>(R Function(T, int i) callback) {
+    List<R> result = [];
+    for (int i = 0; i < length; i++) {
+      R item = callback(this[i], i);
+      result.add(item);
+    }
+    return result;
+  }
+
+  forEachWithIndex(void Function(T, int idx) callback) {
+    for (int i = 0; i < length; i++) {
+      callback(this[i], i);
+    }
+  }
+}
+
+extension JSString on String {
+  /// https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/String/concat
+  String concat(String other) {
+    return this + other;
+  }
+
+  /// https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/String/includes
+  bool includes(String other) => contains(other);
+
+  /// https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/String/trimEnd
+  String trimEnd() => trimRight();
+
+  /// https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/String/trimStart
+  String trimStart() => trimLeft();
+}
+
+extension CuContext on BuildContext {
+  double get width => MediaQuery.of(this).size.width;
+  double get height => MediaQuery.of(this).size.height;
+
+  bool get isDesktop =>
+      Platform.isWindows || Platform.isLinux || Platform.isMacOS;
+  bool get isMobile => Platform.isAndroid || Platform.isIOS;
+  bool get isAndroid => Platform.isAndroid;
+  bool get isIOS => Platform.isIOS;
+  bool get isMacOS => Platform.isMacOS;
+  bool get isWindows => Platform.isWindows;
+  bool get isLinux => Platform.isLinux;
+}
+
+extension CuStringUtil on String {
+  /// Converts a hex, rgb, or rgba color string to Color object. Works with opacity values as well.
+  ///
+  /// e.g.:
+  /// ```
+  ///       "#000"                    -> Color(0xff000000)
+  ///       "#cc3333"                 -> Color(0xffcc3333)
+  ///       "#cc3333dd"               -> Color(0xddcc3333)
+  ///       "rgb(204, 44, 81)"        -> Color(0xffcc2c51)
+  ///       "rgba(204, 44, 81, 0.20)" -> Color(0x33cc2c51)
+  ///       "rgba(204, 44, 81, 0.80)" -> Color(0xcccc2c51)
+  /// ```
+  /// https://gist.github.com/whiplashoo/afe1c8e542c6bf8e8b2659eb9d9a4071
+  Color get toColor {
+    return utils.hexOrRGBToColor(this);
+  }
+
+  String get capitalize {
+    return "${this[0].toUpperCase()}${substring(1)}";
+  }
+
+  bool get isURL => utils.isURL(this);
+
+  bool get isIP => utils.isIP(this);
+
+  bool get isNumber {
+    return double.tryParse(this) != null;
+  }
+}
+
+extension CuColorUtil on Color {
+  /// Returns a hex string representation of the color.
+  ///
+  /// e.g.: Color(0xff000000) -> "#000000"
+  String get toHex {
+    return utils.colorToHex(this);
+  }
+}
 
 extension CuSpaceWidgetDouble on double {
   /// Return self height spacing placeholder widget
---
+++ lib/utils.dart
@@ -0,0 +1,226 @@
+import 'package:flutter/material.dart';
+
+/// https://gist.github.com/whiplashoo/afe1c8e542c6bf8e8b2659eb9d9a4071
+Color hexOrRGBToColor(String colorStr) {
+  RegExp hexColorRegex =
+      RegExp(r'^#?([0-9a-fA-F]{3}|[0-9a-fA-F]{6}|[0-9a-fA-F]{8})$');
+  if (colorStr.startsWith("rgba")) {
+    List rgbaList = colorStr.substring(5, colorStr.length - 1).split(",");
+    return Color.fromRGBO(int.parse(rgbaList[0]), int.parse(rgbaList[1]),
+        int.parse(rgbaList[2]), double.parse(rgbaList[3]));
+  } else if (colorStr.startsWith("rgb")) {
+    List rgbList = colorStr
+        .substring(4, colorStr.length - 1)
+        .split(",")
+        .map((c) => int.parse(c))
+        .toList();
+    return Color.fromRGBO(rgbList[0], rgbList[1], rgbList[2], 1.0);
+  } else if (hexColorRegex.hasMatch(colorStr)) {
+    if (colorStr.length == 4) {
+      colorStr = colorStr + colorStr.substring(1, 4);
+    }
+    if (colorStr.length == 7) {
+      int colorValue = int.parse(colorStr.substring(1), radix: 16);
+      return Color(colorValue).withOpacity(1.0);
+    } else {
+      int colorValue = int.parse(colorStr.substring(1, 7), radix: 16);
+      double opacityValue =
+          int.parse(colorStr.substring(7), radix: 16).toDouble() / 255;
+      return Color(colorValue).withOpacity(opacityValue);
+    }
+  } else if (colorStr.isEmpty) {
+    throw UnsupportedError("Empty color field found.");
+  } else if (colorStr == 'none') {
+    return Colors.transparent;
+  } else {
+    throw UnsupportedError(
+        "Only hex, rgb, or rgba color format currently supported. String:  $colorStr");
+  }
+}
+
+String colorToHex(Color c) {
+  return "#${(c.value.toRadixString(16))..padLeft(8, '0').toUpperCase()}";
+}
+
+_shift(List l) {
+  if (l.isNotEmpty) {
+    var first = l.first;
+    l.removeAt(0);
+    return first;
+  }
+  return null;
+}
+
+RegExp _ipv4Maybe = RegExp(r'^(\d?\d?\d)\.(\d?\d?\d)\.(\d?\d?\d)\.(\d?\d?\d)$');
+RegExp _ipv6 =
+    RegExp(r'^::|^::1|^([a-fA-F0-9]{1,4}::?){1,7}([a-fA-F0-9]{1,4})$');
+
+/// check if the string [str] is IP [version] 4 or 6
+///
+/// * [version] is a String or an `int`.
+bool isIP(String? str, [/*<String | int>*/ version]) {
+  version = version.toString();
+  if (version == 'null') {
+    return isIP(str, 4) || isIP(str, 6);
+  } else if (version == '4') {
+    if (!_ipv4Maybe.hasMatch(str!)) {
+      return false;
+    }
+    var parts = str.split('.');
+    parts.sort((a, b) => int.parse(a) - int.parse(b));
+    return int.parse(parts[3]) <= 255;
+  }
+  return version == '6' && _ipv6.hasMatch(str!);
+}
+
+/// check if the string [str] is a fully qualified domain name (e.g. domain.com).
+///
+/// * [requireTld] sets if TLD is required
+/// * [allowUnderscore] sets if underscores are allowed
+bool isFQDN(String str,
+    {bool requireTld = true, bool allowUnderscores = false}) {
+  var parts = str.split('.');
+  if (requireTld) {
+    var tld = parts.removeLast();
+    if (parts.isEmpty || !RegExp(r'^[a-z]{2,}$').hasMatch(tld)) {
+      return false;
+    }
+  }
+
+  for (var part in parts) {
+    if (allowUnderscores) {
+      if (part.contains('__')) {
+        return false;
+      }
+    }
+    if (!RegExp(r'^[a-z\\u00a1-\\uffff0-9-]+$').hasMatch(part)) {
+      return false;
+    }
+    if (part[0] == '-' ||
+        part[part.length - 1] == '-' ||
+        part.contains('---')) {
+      return false;
+    }
+  }
+  return true;
+}
+
+/// check if the string [str] is a URL
+///
+/// * [protocols] sets the list of allowed protocols
+/// * [requireTld] sets if TLD is required
+/// * [requireProtocol] is a `bool` that sets if protocol is required for validation
+/// * [allowUnderscore] sets if underscores are allowed
+/// * [hostWhitelist] sets the list of allowed hosts
+/// * [hostBlacklist] sets the list of disallowed hosts
+bool isURL(String? str,
+    {List<String?> protocols = const ['http', 'https', 'ftp'],
+    bool requireTld = true,
+    bool requireProtocol = false,
+    bool allowUnderscore = false,
+    List<String> hostWhitelist = const [],
+    List<String> hostBlacklist = const []}) {
+  if (str == null ||
+      str.isEmpty ||
+      str.length > 2083 ||
+      str.startsWith('mailto:')) {
+    return false;
+  }
+
+  var protocol,
+      user,
+      auth,
+      host,
+      hostname,
+      port,
+      portStr,
+      path,
+      query,
+      hash,
+      split;
+
+  // check protocol
+  split = str.split('://');
+  if (split.length > 1) {
+    protocol = _shift(split);
+    if (!protocols.contains(protocol)) {
+      return false;
+    }
+  } else if (requireProtocol == true) {
+    return false;
+  }
+  str = split.join('://');
+
+  // check hash
+  split = str!.split('#');
+  str = _shift(split);
+  hash = split.join('#');
+  if (hash != null && hash != "" && RegExp(r'\s').hasMatch(hash)) {
+    return false;
+  }
+
+  // check query params
+  split = str!.split('?');
+  str = _shift(split);
+  query = split.join('?');
+  if (query != null && query != "" && RegExp(r'\s').hasMatch(query)) {
+    return false;
+  }
+
+  // check path
+  split = str!.split('/');
+  str = _shift(split);
+  path = split.join('/');
+  if (path != null && path != "" && RegExp(r'\s').hasMatch(path)) {
+    return false;
+  }
+
+  // check auth type urls
+  split = str!.split('@');
+  if (split.length > 1) {
+    auth = _shift(split);
+    if (auth.indexOf(':') >= 0) {
+      auth = auth.split(':');
+      user = _shift(auth);
+      if (!RegExp(r'^\S+$').hasMatch(user)) {
+        return false;
+      }
+      if (!RegExp(r'^\S*$').hasMatch(user)) {
+        return false;
+      }
+    }
+  }
+
+  // check hostname
+  hostname = split.join('@');
+  split = hostname.split(':');
+  host = _shift(split);
+  if (split.length > 0) {
+    portStr = split.join(':');
+    try {
+      port = int.parse(portStr, radix: 10);
+    } catch (e) {
+      return false;
+    }
+    if (!RegExp(r'^[0-9]+$').hasMatch(portStr) || port <= 0 || port > 65535) {
+      return false;
+    }
+  }
+
+  if (!isIP(host) &&
+      !isFQDN(host,
+          requireTld: requireTld, allowUnderscores: allowUnderscore) &&
+      host != 'localhost') {
+    return false;
+  }
+
+  if (hostWhitelist.isNotEmpty && !hostWhitelist.contains(host)) {
+    return false;
+  }
+
+  if (hostBlacklist.isNotEmpty && hostBlacklist.contains(host)) {
+    return false;
+  }
+
+  return true;
+}
